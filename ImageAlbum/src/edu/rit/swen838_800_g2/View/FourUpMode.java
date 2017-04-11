4a44a285823c2cfa2fcbae97fbed16633ba7e32d
rpoturich <rxp2553@g.rit.edu>
2017-04-11 22:23:56 +0200

    Added ViewMode skeleton classes

---
+++ ImageAlbum/src/edu/rit/swen838_800_g2/View/FourUpMode.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rit.swen838_800_g2.View;
+
+/**
+ *
+ * @author rachelpoturich
+ */
+public class FourUpMode extends ViewMode{
+    
+    
+    @Override
+    public void loadImages(){}
+    
+}
---
+++ ImageAlbum/src/edu/rit/swen838_800_g2/View/ViewMode.java
@@ -0,0 +1,13 @@
+
+package edu.rit.swen838_800_g2.View;
+
+/**
+ *
+ * @author rachelpoturich
+ */
+public abstract class ViewMode {
+    
+    
+    public abstract void loadImages();
+    
+}

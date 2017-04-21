276d11a2b5c73e978e66fdba9c7c7097c9a87b3e
Mirko <Mirko@RITcroatia>
2017-04-21 18:44:09 +0200

    modified crop function (ex. resize) crop function

--- ImageAlbum/src/ImageAlbumTest.java
+++ ImageAlbum/src/ImageAlbumTest.java
@@ -32,6 +32,8 @@ public class ImageAlbumTest {
 
     Stack<String> comands = new Stack<>();
 
+    Stack<String> redoCommands = new Stack<>();
+
     BufferedImage image;
 
     int currentImg = 0;
@@ -87,8 +89,9 @@ public class ImageAlbumTest {
         JButton jbRotateRight = new JButton("rotate right");
         JButton jbMirror = new JButton("mirror");
         JButton jbFlip = new JButton("flip");
-        JButton jbResize = new JButton("resize");
+        JButton jbCrop = new JButton("Crop");
         JButton jbUndo = new JButton("Undo");
+        JButton jbRedo = new JButton("Redo");
 
         southPanel.add(jbPrev);
         southPanel.add(jbNext);
@@ -97,8 +100,9 @@ public class ImageAlbumTest {
         southPanel.add(jbRotateRight);
         southPanel.add(jbMirror);
         southPanel.add(jbFlip);
-        southPanel.add(jbResize);
+        southPanel.add(jbCrop);
         southPanel.add(jbUndo);
+        southPanel.add(jbRedo);
 
         jbRotateLeft.addActionListener(new ActionListener() {
             @Override
@@ -276,15 +280,15 @@ public class ImageAlbumTest {
             }
         });
 
-        jbResize.addActionListener(new ActionListener() {
+        jbCrop.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                comands.push(jbResize.getText());
+                comands.push(jbCrop.getText());
 
                 myFrame.setEnabled(false);
 
-                JFrame newFrame = new JFrame("resize");
+                JFrame newFrame = new JFrame("crop");
                 newFrame.setLayout(new BorderLayout());
 
                 JLabel label = new JLabel("X coords: ");
@@ -309,19 +313,19 @@ public class ImageAlbumTest {
 //
 //                newFrame.add(panelCenter, BorderLayout.CENTER);
                 JPanel panelCenter = new JPanel(new GridLayout(4, 2));
-                 panelCenter.add(label);
-                 panelCenter.add(x);
-                 panelCenter.add(label1);
-                 panelCenter.add(y);
-                 panelCenter.add(label2);
-                 panelCenter.add(width);
-                 panelCenter.add(label3);
-                 panelCenter.add(heigth);
-                 
-                 newFrame.add(panelCenter, BorderLayout.NORTH);
+                panelCenter.add(label);
+                panelCenter.add(x);
+                panelCenter.add(label1);
+                panelCenter.add(y);
+                panelCenter.add(label2);
+                panelCenter.add(width);
+                panelCenter.add(label3);
+                panelCenter.add(heigth);
+
+                newFrame.add(panelCenter, BorderLayout.NORTH);
 
                 JPanel panelSouth = new JPanel();
-                JButton button = new JButton("Resize");
+                JButton button = new JButton("crop");
                 panelSouth.add(button);
                 newFrame.add(panelSouth, BorderLayout.SOUTH);
 
@@ -334,29 +338,35 @@ public class ImageAlbumTest {
 
                         myFrame.setEnabled(true);
                         newFrame.setVisible(false);
-                        
-                        try{
-                        BufferedImage originalImgage = ImageIO.read(new File(imagePath.get(currentImg)));
 
-			
-			System.out.println("Original Image Dimension: "+originalImgage.getWidth()+"x"+originalImgage.getHeight());
+                        try {
+                            BufferedImage originalImgage = ImageIO.read(new File(imagePath.get(currentImg)));
 
-			BufferedImage SubImgage = originalImgage.getSubimage(Integer.parseInt(x.getText()), Integer.parseInt(y.getText()), 
-                                                                   Integer.parseInt(width.getText()),Integer.parseInt(heigth.getText()));
-			System.out.println("Cropped Image Dimension: "+SubImgage.getWidth()+"x"+SubImgage.getHeight());
+                            System.out.println("Original Image Dimension: " + originalImgage.getWidth() + "x" + originalImgage.getHeight());
 
-			File outputfile = new File(imagePath.get(currentImg));
-			ImageIO.write(SubImgage, "jpg", outputfile);
+                            BufferedImage SubImgage = originalImgage.getSubimage(Integer.parseInt(x.getText()), Integer.parseInt(y.getText()),
+                                    Integer.parseInt(width.getText()), Integer.parseInt(heigth.getText()));
+                            System.out.println("Cropped Image Dimension: " + SubImgage.getWidth() + "x" + SubImgage.getHeight());
 
-			System.out.println("Image cropped successfully: "+outputfile.getPath());
-                        }catch(IOException ioe){
+                            //    File outputfile = new File(imagePath.get(currentImg) + ".pngCropied");
+                            ImageIO.write(SubImgage, "png", new File(imagePath.get(currentImg) + ".pngCropied"));
+
+                            image = ImageIO.read(new File(imagePath.get(currentImg) + ".pngCropied"));
+
+                            imagePath.add(currentImg, imagePath.get(currentImg) + ".pngCropied");
+
+                            imagePath.remove(currentImg + 1);
+
+                            centerPanel.repaint();
+
+//sdfgdsfgsdfgsdfgsdfgsdfgdsfgdsfgdsfg
+                            System.out.println("Image cropped successfully: " + imagePath.get(currentImg));
+                        } catch (IOException ioe) {
                             ioe.printStackTrace();
                         }
 
                         //Graphics2D g2d = image.createGraphics();
-
                         //g2d.drawImage(image, 0, 0, Integer.parseInt(width.getText()), Integer.parseInt(heigth.getText()), null);
-
                         centerPanel.repaint();
 
                     }
@@ -382,6 +392,8 @@ public class ImageAlbumTest {
                 try {
                     String lastCommand = comands.pop();
                     System.out.println(lastCommand);
+
+                    redoCommands.add(lastCommand);
 
                     if (lastCommand.equalsIgnoreCase(jbFlip.getText())) {
 
@@ -489,6 +501,16 @@ public class ImageAlbumTest {
 
                         centerPanel.repaint();
 
+                    }  else if (lastCommand.equalsIgnoreCase(jbCrop.getText())) {
+                        
+                        image = ImageIO.read(new File(imagePath.get(currentImg).substring(0, imagePath.get(currentImg).indexOf(".pngCropied"))));
+
+                        imagePath.add(currentImg, imagePath.get(currentImg).substring(0, imagePath.get(currentImg).indexOf(".pngCropied")));
+                        
+                        imagePath.remove(currentImg + 1);
+                        
+                        centerPanel.repaint();
+                        
                     }
 
                 } catch (EmptyStackException esex) {
@@ -500,6 +522,249 @@ public class ImageAlbumTest {
             }
         });
 
+        jbRedo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String cmdRedo = redoCommands.pop();
+
+                    if (cmdRedo.equalsIgnoreCase(jbFlip.getText())) {
+
+                        comands.push(jbFlip.getText());
+
+                        int height = image.getHeight();
+
+                        AffineTransform at = AffineTransform.getScaleInstance(1, -1);
+
+                        at.translate(0, -height);
+
+                        AffineTransformOp ato = new AffineTransformOp(at, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+
+                        image = ato.filter(image, null);
+
+                        centerPanel.repaint();
+
+                    } else if (cmdRedo.equalsIgnoreCase(jbMirror.getText())) {
+
+                        comands.push(jbMirror.getText());
+
+                        int width = image.getWidth();
+                        int height = image.getHeight();
+
+                        Graphics2D g2d = image.createGraphics();
+
+                        g2d.drawImage(image, width, 0, -width, height, null);
+
+                        centerPanel.repaint();
+
+                    } else if (cmdRedo.equalsIgnoreCase(jbBW.getText())) {
+
+                        try {
+                            if (!(imagePath.get(currentImg).contains(".pngBlackWhite"))) {
+
+                                comands.push(jbBW.getText());
+
+                                BufferedImage BWimage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+
+                                System.out.println(currentImg);
+
+                                Graphics2D g2d = BWimage.createGraphics();
+
+                                g2d.drawImage(image, 0, 0, null);
+
+                                ImageIO.write(BWimage, "png", new File(imagePath.get(currentImg) + ".pngBlackWhite"));
+
+                                image = ImageIO.read(new File(imagePath.get(currentImg) + ".pngBlackWhite"));
+
+                                imagePath.add(currentImg, imagePath.get(currentImg) + ".pngBlackWhite");
+                                imagePath.remove(currentImg + 1);
+
+                                centerPanel.repaint();
+                            }
+                        } catch (IOException ex) {
+                            Logger.getLogger(ImageAlbumTest.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+
+                    } else if (cmdRedo.equalsIgnoreCase(jbNext.getText())) {
+
+                        comands.push(jbNext.getText());
+
+                        currentImg++;
+
+                        if (currentImg > imagePath.size() - 1) {
+                            currentImg = 0;
+                        }
+
+                        try {
+                            image = ImageIO.read(new File(imagePath.get(currentImg)));
+
+                        } catch (IOException ex) {
+                            // TODO Auto-generated catch block
+                            ex.printStackTrace();
+                        }
+                        System.out.println(currentImg);
+
+                        centerPanel.repaint();
+
+                    } else if (cmdRedo.equalsIgnoreCase(jbPrev.getText())) {
+
+                        comands.push(jbPrev.getText());
+
+                        currentImg--;
+                        if (currentImg < 0) {
+                            currentImg = imagePath.size() - 1;
+                        }
+
+                        try {
+                            image = ImageIO.read(new File(imagePath.get(currentImg)));
+
+                        } catch (IOException ex) {
+                            // TODO Auto-generated catch block
+                            ex.printStackTrace();
+                        }
+                        System.out.println(currentImg);
+
+                        centerPanel.repaint();
+
+                    } else if (cmdRedo.equalsIgnoreCase(jbRotateLeft.getText())) {
+
+                        comands.push(jbRotateLeft.getText());
+
+                        image = rotateImage(image, 90);
+                        int drawLocationX = 0;
+                        int drawLocationY = 0;
+
+                        // Rotation information
+                        AffineTransform tx = AffineTransform.getRotateInstance(Math.toRadians(90), image.getWidth() / 2, image.getHeight() / 2);
+                        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+
+                        Graphics2D g2d = image.createGraphics();
+
+                        // Drawing the rotated image at the required drawing locations
+                        g2d.drawImage(op.filter(image, null), drawLocationX, drawLocationY, null);
+
+                        centerPanel.repaint();
+
+                    } else if (cmdRedo.equalsIgnoreCase(jbRotateRight.getText())) {
+
+                        comands.push(jbRotateRight.getText());
+
+                        image = rotateImage(image, -90);
+                        int drawLocationX = 0;
+                        int drawLocationY = 0;
+
+                        // Rotation information
+                        AffineTransform tx = AffineTransform.getRotateInstance(Math.toRadians(-90), image.getWidth() / 2, image.getHeight() / 2);
+                        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+
+                        Graphics2D g2d = image.createGraphics();
+
+                        // Drawing the rotated image at the required drawing locations
+                        g2d.drawImage(op.filter(image, null), drawLocationX, drawLocationY, null);
+
+                        centerPanel.repaint();
+
+                    } else if (cmdRedo.equalsIgnoreCase(jbCrop.getText())) {
+
+                        comands.push(jbCrop.getText());
+
+                        myFrame.setEnabled(false);
+
+                        JFrame newFrame = new JFrame("crop");
+                        newFrame.setLayout(new BorderLayout());
+
+                        JLabel label = new JLabel("X coords: ");
+                        JLabel label1 = new JLabel("Y coords: ");
+                        JLabel label2 = new JLabel("Width: ");
+                        JLabel label3 = new JLabel("Heigth: ");
+
+                        JTextField x = new JTextField(5);
+                        JTextField y = new JTextField(5);
+                        JTextField width = new JTextField(5);
+                        JTextField heigth = new JTextField(5);
+
+//                JPanel panelNorth = new JPanel();
+//                panelNorth.add(label);
+//                panelNorth.add(width);
+//
+//                newFrame.add(panelNorth, BorderLayout.NORTH);
+//
+//                JPanel panelCenter = new JPanel();
+//                panelCenter.add(label1);
+//                panelCenter.add(heigth);
+//
+//                newFrame.add(panelCenter, BorderLayout.CENTER);
+                        JPanel panelCenter = new JPanel(new GridLayout(4, 2));
+                        panelCenter.add(label);
+                        panelCenter.add(x);
+                        panelCenter.add(label1);
+                        panelCenter.add(y);
+                        panelCenter.add(label2);
+                        panelCenter.add(width);
+                        panelCenter.add(label3);
+                        panelCenter.add(heigth);
+
+                        newFrame.add(panelCenter, BorderLayout.NORTH);
+
+                        JPanel panelSouth = new JPanel();
+                        JButton button = new JButton("crop");
+                        panelSouth.add(button);
+                        newFrame.add(panelSouth, BorderLayout.SOUTH);
+
+                        newFrame.pack();
+
+                        button.addActionListener(new ActionListener() {
+
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+
+                                myFrame.setEnabled(true);
+                                newFrame.setVisible(false);
+
+                                try {
+                                    BufferedImage originalImgage = ImageIO.read(new File(imagePath.get(currentImg)));
+
+                                    System.out.println("Original Image Dimension: " + originalImgage.getWidth() + "x" + originalImgage.getHeight());
+
+                                    BufferedImage SubImgage = originalImgage.getSubimage(Integer.parseInt(x.getText()), Integer.parseInt(y.getText()),
+                                            Integer.parseInt(width.getText()), Integer.parseInt(heigth.getText()));
+                                    System.out.println("Cropped Image Dimension: " + SubImgage.getWidth() + "x" + SubImgage.getHeight());
+
+                                    //    File outputfile = new File(imagePath.get(currentImg) + ".pngCropied");
+                                    ImageIO.write(SubImgage, "png", new File(imagePath.get(currentImg) + ".pngCropied"));
+
+                                    image = ImageIO.read(new File(imagePath.get(currentImg) + ".pngCropied"));
+
+                                    imagePath.add(currentImg, imagePath.get(currentImg) + ".pngCropied");
+
+                                    imagePath.remove(currentImg + 1);
+
+                                    centerPanel.repaint();
+
+//sdfgdsfgsdfgsdfgsdfgsdfgdsfgdsfgdsfg
+                                    System.out.println("Image cropped successfully: " + imagePath.get(currentImg));
+                                } catch (IOException ioe) {
+                                    ioe.printStackTrace();
+                                }
+
+                                //Graphics2D g2d = image.createGraphics();
+                                //g2d.drawImage(image, 0, 0, Integer.parseInt(width.getText()), Integer.parseInt(heigth.getText()), null);
+                                centerPanel.repaint();
+
+                            }
+
+                        });
+
+                        newFrame.setLocationRelativeTo(null);
+                        newFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                        newFrame.setVisible(true);
+
+                    }
+                } catch (EmptyStackException esex) {
+                    System.out.println("No more redo");
+                }
+            }
+        });
         centerPanel.repaint();
         System.out.println(image);
 
@@ -533,7 +798,7 @@ public class ImageAlbumTest {
 
         for (int i = 0; i < imagePath.size(); i++) {
 
-            if (imagePath.get(i).contains(".pngBlackWhite")) {
+            if (imagePath.get(i).contains(".pngBlackWhite") || imagePath.get(i).contains(".pngCropied")) {
                 imagePathToDelete.add(imagePath.get(i));
                 imagePath.remove(i);
             }

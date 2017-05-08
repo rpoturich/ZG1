d9708c20dadfc2514c071263e22eeb6292828dac
rpoturich <rxp2553@g.rit.edu>
2017-05-08 09:42:30 +0200

    DataSaver commit attempt

---
+++ ImageAlbum/src/edu/rit/swen383_800_g2/DataSaver/DataSaver.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rit.swen383_800_g2.DataSaver;
+
+/**
+ *
+ * @author rachelpoturich
+ */
+public interface DataSaver {
+    
+    
+}
